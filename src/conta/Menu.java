bbac2ea21b9b5a2ab515e4e22bd17f1117734b8b
kauanhassani <kauanhassani@gmail.com>
2023-06-05 17:39:37 -0300

    Atualizações da Conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,17 +2,44 @@ package conta;
 
 import java.util.Scanner;
 import conta.util.Cores;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
+
+		// pra testar a Classe Conta
+		Conta c1 = new Conta(1, 001, 1, "Marinette", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+
+		// Teste da Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(3, 002, 1, "Ruanatan", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(5, 003, 2, "Luiza Mell (com z)", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
 
 		while (true) {
-			
+
 			System.out.println(Cores.TEXT_GREEN_BOLD + Cores.ANSI_BLACK_BACKGROUND
 					+ "****************************************************");
 			System.out.println("                                                    ");
@@ -51,43 +78,42 @@ public class Menu {
 
 			case 2:
 				System.out.println(Cores.TEXT_WHITE + "Listar todas as contas\n\n");
-				
+
 				break;
-				
+
 			case 3:
 				System.out.println(Cores.TEXT_WHITE + "Buscar conta por número\n\n");
-				
+
 				break;
-				
+
 			case 4:
 				System.out.println(Cores.TEXT_WHITE + "Atualizar Dados da conta - por um número\n\n");
-				
+
 				break;
-				
+
 			case 5:
 				System.out.println(Cores.TEXT_WHITE + "Apagar conta\n\n");
-				
+
 				break;
-				
+
 			case 6:
 				System.out.println(Cores.TEXT_WHITE + "Sacar\n\n");
-				
+
 				break;
-				
+
 			case 7:
 				System.out.println(Cores.TEXT_WHITE + "Depositar\n\n");
-				
+
 				break;
-				
+
 			case 8:
 				System.out.println(Cores.TEXT_WHITE + "Transferir valores entre contas\n\n");
-				
+
 				break;
-				
+
 			case 9:
 				System.out.println("Sair\n\n" + Cores.TEXT_RESET);
-				
-				
+
 				break;
 			}
 
